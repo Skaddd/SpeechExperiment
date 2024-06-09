55137ec30afbf7374301b6b19db8e07d5921f0db
skaddd <mateo.lebrun@telecomnancy.net>
2024-06-09 14:58:47 +0200

    Removed peft callback because unefficient + small tweaks on parameters.

--- src/utils/prep_utils.py
+++ src/utils/prep_utils.py
@@ -1,15 +1,6 @@
 import torch
-import os
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
-
-from transformers import (
-    TrainerCallback,
-    TrainingArguments,
-    TrainerState,
-    TrainerControl,
-)
-from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
 
 
 @dataclass
@@ -57,28 +48,3 @@ class DataCollatorSpeechSeq2SeqWithPadding:
         batch["labels"] = labels
 
         return batch
-
-
-# This callback helps to save only the adapter weights and remove t
-# he base model weights.
-class SavePeftModelCallback(TrainerCallback):
-    def on_save(
-        self,
-        args: TrainingArguments,
-        state: TrainerState,
-        control: TrainerControl,
-        **kwargs,
-    ):
-        checkpoint_folder = os.path.join(
-            args.output_dir, f"{PREFIX_CHECKPOINT_DIR}-{state.global_step}"
-        )
-
-        peft_model_path = os.path.join(checkpoint_folder, "adapter_model")
-        kwargs["model"].save_pretrained(peft_model_path)
-
-        pytorch_model_path = os.path.join(
-            checkpoint_folder, "pytorch_model.bin"
-        )
-        if os.path.exists(pytorch_model_path):
-            os.remove(pytorch_model_path)
-        return control
